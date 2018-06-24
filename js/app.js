94eaab2a7ed92820f5939ac9d9622293e70b9c93
Nicholas Potter <pottern12@gmail.com>
2018-06-24 13:16:28 -0500

    added animations

--- js/app.js
+++ js/app.js
@@ -4,6 +4,8 @@ const cardList = ["fa fa-diamond", "fa fa-paper-plane-o", "fa fa-anchor",
 "fa fa-paper-plane-o", "fa fa-anchor", "fa fa-bolt", "fa fa-cube", "fa fa-leaf", 
 "fa fa-bicycle", "fa fa-bomb"];
 match=0;
+
+//initialize game board
 for (let i = 0; i <= cardList.length-1; i++) {
     $('.deck').append('<li><i>');
 }
@@ -12,6 +14,7 @@ function emptyArray() {
             cards.length=0;
         };
 
+//add card class to game board elements
 const startGame = function() {
         let cardNumber = 0;
         $('.deck').each(function() {
@@ -26,23 +29,30 @@ const startGame = function() {
     });
 };
 
+//check number of moves remaining and gives star rating
 const starRating = function (){
     if (moves <= 8) {
         $('.three').removeClass('fa-star').addClass('fa-star-o');
-    }
+    };
     if (moves <= 4) {
         $('.two').removeClass('fa-star').addClass('fa-star-o');
-    }
+    };
     if (moves === 0) {
         $('.one').removeClass('fa-star').addClass('fa-star-o');
-        $('section').text('GAME OVER!').css({
+        $('section').text('GAME OVER!').addClass('animated fadeInUp').css({
             textAlign: 'center',
             fontSize: '20px',
-            fontWeight: 'bold'
         });
-    }
-}
+        $('h1').text('YOU LOSE').addClass('animated fadeInDown');
+        $('.deck').addClass('animated bounceOut').css({
+            background: 'linear-gradient(160deg, #ff7270 0%, #ea4527 80%)',
+            property2: 'value2'
+        });
+    };
+};
 
+//add event listeners to cards to be flipped
+//check for match or mismatch and adds/removes appropriate classes
 const addCardListener = function(){
     $('.deck').find('.card').bind('click', function() {
         let card = $(this).attr('class');
@@ -50,17 +60,17 @@ const addCardListener = function(){
         cards.push(card);
         if (cards.length > 1) {
             if (card === cards[0]) {
-                $('.deck').find('.open').addClass('match');
+                $('.deck').find('.open').addClass('animated bounce match');
                 setTimeout(function (){
-                    $('.deck').find('.match').removeClass('open show');
-                }, 600);
+                    $('.deck').find('.match').removeClass('animated bounce open show');
+                }, 1000);
                 match++;
                 emptyArray();
             } else {
-                $('.deck').find('.open').addClass('notmatch');
+                $('.deck').find('.show').removeClass('open').addClass('animated tada notmatch');
                 setTimeout(function (){
-                    $('.deck').find('.notmatch').removeClass('open show notmatch');
-                }, 600)
+                    $('.deck').find('.notmatch').removeClass('animated tada open show notmatch');
+                }, 1000)
                 moves--;
                 $('.moves').text(moves);
                 emptyArray();
@@ -71,15 +81,18 @@ const addCardListener = function(){
     }});
 };
 
+//check number of matches to determine victory condition
 const endGame = function(){
     if (match==8) {
         $('.deck').find('.match').removeClass('open show match').addClass('winner');
     }};
 
+//reinitialize game board with restart button
 $('.restart').bind('click', function() {
     startGame();
 });
 
+//shuffle function
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
 
@@ -97,11 +110,3 @@ shuffle(cardList);
 startGame();
 addCardListener();
 starRating();
-
-/*
- *    + increment the move counter and display it on the page (put this 
-        functionality in another function that you call from this one)
-
- *    + if all cards have matched, display a message with the final score 
-        (put this functionality in another function that you call from this one)
- */
